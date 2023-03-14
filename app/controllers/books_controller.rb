b2bab7f60b6374b226a7d356756736dde4d2c5a5
daze573 <124431662+daze573@users.noreply.github.com>
2023-03-14 10:25:16 +0000

    [dineer]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,10 +2,14 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
+    @book_new = Book.new
   end
 
   def index
     @books = Book.all
+    @book = Book.new
+    @user = @book.user
   end
 
   def create
@@ -28,11 +32,12 @@ class BooksController < ApplicationController
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
+      @book = Book.find(parmas[:id])
       render "edit"
     end
   end
 
-  def delete
+  def destoy
     @book = Book.find(params[:id])
     @book.destoy
     redirect_to books_path
@@ -41,6 +46,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 end
