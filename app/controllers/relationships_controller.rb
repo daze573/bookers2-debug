2ae3b6b29db2f4243403e3fcb7252b8db49a764f
daze573 <124431662+daze573@users.noreply.github.com>
2023-03-16 14:09:22 +0000

    [Finish] 応用課題 1-4

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,21 +2,21 @@ class RelationshipsController < ApplicationController
 
   def create
     current_user.follow(params[:user_id])
-    # redirect_to users_path
+    redirect_to user_path(params[:user_id])
   end
 
   def destroy
     current_user.unfollow(params[:user_id])
-    # redirect_to users_path
+    redirect_to user_path(params[:user_id])
   end
 
   def followings
-    user = user.find(params[:user_id])
+    user = User.find(params[:id])
     @users = user.followings
   end
 
   def followers
-    user = User.find(params[:user_id])
+    user = User.find(params[:id])
     @users = user.followers
   end
 end
