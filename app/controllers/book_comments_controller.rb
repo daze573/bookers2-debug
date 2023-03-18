2b41b1f8e2cde2608fdd73a72b6d27baad42522f
daze573 <124431662+daze573@users.noreply.github.com>
2023-03-18 08:26:00 +0000

    [休憩]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,12 +5,14 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = @book.id
     comment.save
-    redirect_to book_path(@book)
+    render :create
+    # redirect_to book_path(@book)
   end
 
   def destroy
     BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    render :destroy
+    # redirect_to book_path(params[:book_id])
   end
 
   private
