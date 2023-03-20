c2bb5cb15629c8dd6f750e666b9688fb4a825367
daze573 <124431662+daze573@users.noreply.github.com>
2023-03-20 06:28:31 +0000

    [応用課題 8aまで完了]

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,4 +1,6 @@
 class ChatsController < ApplicationController
+  before_action :reject_non_related, only: [:show]
+
   def show
     @user = User.find(params[:id])
     rooms = current_user.user_rooms.pluck(:room_id)
@@ -19,12 +21,21 @@ class ChatsController < ApplicationController
 
   def create
     @chat = current_user.chats.new(chat_params)
-    @chat.save
+    unless @chat.save
+      render 'error'
+    end
   end
 
   private
 
   def chat_params
     params.require(:chat).permit(:message, :room_id)
+  end
+
+  def reject_non_related
+    user = User.find(params[:id])
+    unless current_user.following?(user) && user.following?(current_user)
+      redirect_to books_path
+    end
   end
 end
